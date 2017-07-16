ce0d3a7d8d08225a480da813c829aeeb3c0ccf55
Michael Devery <michaeldevery@gmail.com>
2017-07-16 01:11:51 +0100

    Remove ng-route, views, move everything to app.js and index.html

--- app/app.js
+++ app/app.js
@@ -1,14 +1,64 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+// Declare app
+angular.module('namePicker', [])
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+.controller('mainCtrl', ['$scope', function($scope) {
+
+  $scope.sentences = [];
+  $scope.words = [];
+  $scope.newVariants = [];
+  $scope.selectedVariants = [];
+
+  $scope.addSentence = function() {
+    if (!inArray($scope.newSentence, $scope.sentences)) {
+      $scope.sentences.push($scope.newSentence);
+    } else {
+      // alert('Duplicate sentence');
+    }
+    $scope.newSentence = '';
+  };
+
+  $scope.addWord = function() {
+    var key = $scope.words.push([$scope.newWord]);
+    $scope.selectedVariants[key - 1] = $scope.newWord; // select new word
+    $scope.newWord = '';
+  };
+
+  $scope.addVariant = function(key) {
+    var newVariant = $scope.newVariants[key];
+    if (!inArray(newVariant, $scope.words[key])) {
+      $scope.words[key].push(newVariant);
+      $scope.selectedVariants[key] = newVariant; // select new word
+    } else {
+      // alert('Duplicate word variant');
+    }
+    $scope.newVariants[key] = '';
+  };
+
+  function inArray(value, array) {
+    return -1 !== array.indexOf(value);
+  }
+}])
+
+.filter('replaceWords', function() {
+  return function(sentence, selectedVariants) {
+    for (var key in selectedVariants) {
+      sentence = sentence.replace('<' + (parseInt(key) + 1) + '>', selectedVariants[key]);
+    }
+    return sentence;
+  };
+})
+
+.directive('ngEnter', function() {
+  return function(scope, element, attrs) {
+    element.bind("keydown keypress", function(event) {
+      if(event.which === 13) {
+        scope.$apply(function(){
+          scope.$eval(attrs.ngEnter);
+        });
+        event.preventDefault();
+      }
+    });
+  };
+});
